1c49f7873ca2be0d4d327851802bd760bbb2cecf
tail <vclp@163.com>
2020-02-29 01:42:39 +0800

    axios拦截器 接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios';
 import router from './router';
+import App from './App.vue'
+// 跨域方式做调整
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+// 接口错误拦截
+axios.interceptors.response.use(function(responese){
+  let res = responese.data;
+  if(res.status == 0) {
+    return res.data;
+  }else if(res.status == 10){
+    window.Location.href = '/#/login'
+  }else{
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
