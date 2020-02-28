4814a43461fefb196b725af918cc2ea570732b74
tail <vclp@163.com>
2020-02-29 02:36:19 +0800

    环境设置

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import axios from 'axios'
 import VueAxios from 'vue-axios';
 import router from './router';
 import App from './App.vue'
+import env from './env'
 // 跨域方式做调整
 axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
+axios.defaults.baseURL = env.baseURL;
 // 接口错误拦截
 axios.interceptors.response.use(function(responese){
   let res = responese.data;
