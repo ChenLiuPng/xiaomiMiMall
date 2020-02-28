205cc725f1f312b4a38f690a090574be4257a674
tail <vclp@163.com>
2020-02-29 00:48:06 +0800

    1.安装依赖 2.路由封装

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
-        <nav-header></nav-header>
+        <order-header></order-header>
         <router-view></router-view>
         <nav-footer></nav-footer>
     </div>
 </template>
 
 <script>
-import NavHeader from '../components/NavHeader';
-import NavFooter from '../components/NavFooter';
+import OrderHeader from './../components/OrderHeader';
+import NavFooter from './../components/NavFooter';
 export default {
-    name: 'nav-header'
+    name: 'nav-header',
+    components:{
+        OrderHeader,
+        NavFooter
+    }
 }
 </script>
 
